9a37ede9803171a2c80a6e020fc3b595f72e0c19
Auorange <1539530456@qq.com>
2025-01-23 14:19:13 +0000

    dustributed-lock

---
+++ redis/DistributedLock/DistributedLock-example-1/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+func GetRedisConnection() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+	return client
+}
