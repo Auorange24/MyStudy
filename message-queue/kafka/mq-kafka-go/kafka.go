8050cbd9d28ca5c6cac1e7c39d74767703d077e3
Auorange <1539530456@qq.com>
2025-05-27 14:02:42 +0800

    kafka:kafka int

---
+++ message-queue/kafka/mq-kafka-go/kafka.go
@@ -0,0 +1,34 @@
+package mq_kafka_go
+
+import (
+	"context"
+	"log"
+	"github.com/segmentio/kafka-go"
+)
+
+func fatalError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func WriteByConn() {
+	topic := "my-topic" // 主题
+	partition := 0      // 分区
+
+	// 连接到Kafka的集群节点
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	fatalError(err)
+
+	// 设置发送消息的超时时间
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("Hello, World!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
---
+++ message-queue/kafka/mq-kafka-go/kafka_test.go
@@ -0,0 +1,7 @@
+package mq_kafka_go
+
+import "testing"
+
+func TestWriteByConn(t *testing.T) {
+	WriteByConn()
+}
